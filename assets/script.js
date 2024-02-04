3c5a51d7192f8ffac156b46881fe448ed00a8a6c
Benjamin Brechter <benbrechter3@gmail.com>
2024-02-04 16:52:41 -0600

    I grabbed all the elements from the HTML

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,23 @@
 var cityDisplay = document.querySelector('#city-display');
-
-
+var tempDisplay = document.querySelector('#temp-display');
+var windDisplay = document.querySelector('#wind-display');
+var humidityDisplay = document.querySelector('#humidity-display');
 
 let apiKey = '0a4a3a2ba95dc7f27ed0c81a22a94389';
 let city = 'Houston';
+let units = 'standard';
 
-let queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
+let queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey + "&units=" + units;
 
 fetch(queryURL)
 .then(response => response.json())
 .then(json => console.log(json))
 
+
+
+
+
+
+//var userCity = function(city){
+
+//}
